45b6cdb04160550fe703e5712c2b3c7de831f7da
AnKun <153471503@qq.com>
2018-11-09 14:47:48 +0800

    调整了代码结构

--- train.py
+++ train.py
@@ -1,41 +1,11 @@
 import mxnet as mx
-import numpy as np
 import logging
-import pickle
+from tools import *
 
 logging.getLogger().setLevel(logging.DEBUG)
 
-
-def load_cifar10():
-	data = []
-	labels = []
-
-	for num in range(1, 6):
-		with open('./data/data_batch_' + str(num), 'rb') as fo:
-			dict = pickle.load(fo, encoding='bytes')
-			data.append(np.array(dict[b'data']))
-			labels.append(np.array(dict[b'labels']))
-			fo.close()
-
-	with open('./data/test_batch', 'rb') as fo:
-		dict = pickle.load(fo, encoding='bytes')
-		eval_data = np.array(dict[b'data']).reshape(10000, 3072)
-		eval_labels = np.array(dict[b'labels']).reshape(10000)
-		fo.close()
-		
-	train_data = np.array(data).reshape((50000, 3072))
-	train_labels = np.array(labels).reshape(50000)
-	
-	return (train_data, train_labels), (eval_data, eval_labels)
-
-
-def to4d(img):
-	return img.reshape(img.shape[0], 3, 32, 32).astype(np.float32) / 255
-
-
 epoch = 60
 batch_size = 32
-
 
 # 搭建网络
 data = mx.sym.Variable('data')
